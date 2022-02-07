b299b47f13c0fc733354160a79017725a3bd2b5d
srv-shivam <shivamsrivastava222001@gmail.com>
2022-02-07 18:15:38 +0530

    Working on listView.

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
       ),
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -40,111 +41,113 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: [
           const SizedBox(
-            height: 24,
+            height: 20,
           ),
-          Container(
-            height: 140,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  // crossAxisAlignment: CrossAxisAlignment.end,
-                  // This row is for arrangement of My Journal and right side 2 icons in row
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      //This column is for arrangement of Today & My Journal text
-                      children: [
-                        const Text(
-                          "Today",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            fontStyle: FontStyle.normal,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 6,
-                        ),
-                        Row(
-                          children: const [
-                            // TODO::Change it to TextField
-                            Text(
-                              "My Journal",
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600,
-                                fontStyle: FontStyle.normal,
-                                color: Colors.indigo,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Icon(
-                              Icons.edit_outlined,
-                              size: 21,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.school_rounded,
-                          size: 26,
-                        ),
-                        SizedBox(
-                          width: 14,
-                        ),
-                        Icon(
-                          Icons.add_chart,
-                          size: 26,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                ///TODO::Edit here for listview
-                Wrap(
-                  children: [
-                    Container(
-                      color: Colors.indigoAccent,
-                      child: Row(
+          Card(
+            elevation: 1,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // crossAxisAlignment: CrossAxisAlignment.end,
+                    // This row is for arrangement of My Journal and right side 2 icons in row
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        //This column is for arrangement of Today & My Journal text
                         children: [
-                          Icon(
-                            Icons.library_books_outlined,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                            child: Text(
-                              "All Habits",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                              ),
+                          const Text(
+                            "Today",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              fontStyle: FontStyle.normal,
+                              color: Colors.grey,
                             ),
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Row(
+                            children: const [
+                              // TODO::Change it to TextField
+                              Text(
+                                "My Journal",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600,
+                                  fontStyle: FontStyle.normal,
+                                  color: Colors.indigo,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                Icons.edit_outlined,
+                                size: 21,
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.school_rounded,
+                            size: 26,
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          Icon(
+                            Icons.add_chart,
+                            size: 26,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  ///TODO::Edit here for listview
+                  Wrap(
+                    children: [
+                      Container(
+                        color: Colors.indigoAccent,
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.library_books_outlined,
+                              size: 20,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              child: Text(
+                                "All Habits",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
