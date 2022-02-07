11755d8045ec4c0ea4b140c49090c94f15750f53
srv-shivam <shivamsrivastava222001@gmail.com>
2022-02-07 16:50:12 +0530

    Status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,40 +10,88 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
     return MaterialApp(
       title: 'Habitify',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Journal Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          const Center(child: Text("Hello!! I am here to Habitify your taks.")),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            height: 140,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red,
+            padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+            child: Row(
+              // This row is for arrangement of My Journal and right side 2 icons in row
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  //This column is for arrangement of Today & My Journal text
+                  children: [
+                    const Text(
+                      "Today",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    // TODO::Change it to TextField
+                    Row(
+                      children: const [
+                        Text(
+                          "My Journal",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600,
+                            fontStyle: FontStyle.normal,
+                            color: Colors.indigo,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(
+                          Icons.edit_outlined,
+                          size: 25,
+                        ),
+                      ],
+                    ),
+                  ],
+                ), // My Journal
+              ],
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
